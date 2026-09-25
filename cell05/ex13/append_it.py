5cf37e7356b559e336041b14658faae7dba4900c
Supakorn <66070311@kmitl.ac.th>
2026-09-25 12:20:24 +0700

    commit cell05

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -1,9 +1,8 @@
 import sys
-if len(sys.argv) < 2:
+
+if len(sys.argv) <= 1:
     print("none")
 else:
-    matching_params = [param for param in sys.argv[2:] if param.endswith("ism")]
-    if matching_params:
-        print(", ".join(matching_params))
-    else:
-        print("none")
+    for arg in sys.argv[1:]:
+        if not arg.endswith("ism"):
+            print(f"{arg}ism")
